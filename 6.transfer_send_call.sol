a4b742d7a066bd4fd8b44cefdb44a1e3839d01b2
Hari Pandey <69706767+Harry-code21@users.noreply.github.com>
2022-05-25 06:09:21 -0700

    Update and rename transfer_send_call.txt to 6.transfer_send_call.sol

---
+++ 6.transfer_send_call.sol
@@ -0,0 +1,51 @@
+You can send Ether to other contracts by
+
+**   transfer (2300 gas, throws error)
+**   send (2300 gas, returns bool)
+**   call (forward all gas or set gas, returns bool)
+How to receive Ether?
+A contract receiving Ether must have at least one of the functions below :
+          receive() external payable
+          fallback() external payable
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Error {
+    function testRequire(uint _i) public pure {
+        // Require should be used to validate conditions such as:
+        // - inputs
+        // - conditions before execution
+        // - return values from calls to other functions
+        require(_i > 10, "Input must be greater than 10");
+    }
+
+    function testRevert(uint _i) public pure {
+        // Revert is useful when the condition to check is complex.
+        // This code does the exact same thing as the example above
+        if (_i <= 10) {
+            revert("Input must be greater than 10");
+        }
+    }
+
+    uint public num;
+
+    function testAssert() public view {
+        // Assert should only be used to test for internal errors,
+        // and to check invariants.
+
+        // Here we assert that num is always equal to 0
+        // since it is impossible to update the value of num
+        assert(num == 0);
+    }
+
+    // custom error
+    error InsufficientBalance(uint balance, uint withdrawAmount);
+
+    function testCustomError(uint _withdrawAmount) public view {
+        uint bal = address(this).balance;
+        if (bal < _withdrawAmount) {
+            revert InsufficientBalance({balance: bal, withdrawAmount: _withdrawAmount});
+        }
+    }
+}
